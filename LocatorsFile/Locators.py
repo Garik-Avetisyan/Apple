eb9851f81b0f8cd55d76e566fe4051e43322f448
Shoghik Manucharyan <smanucharyan@loyhutz.de>
2022-05-12 22:35:03 +0400

    Shoghik's changes for SelectProductDetailsPage

---
+++ LocatorsFile/Locators.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+# homePageCart = (By.ID, "nav-cart-count")
+navigatinBarMac = (By.XPATH, "(//a[@href='/mac/'])[1]")
+navigatinBarIpad = (By.XPATH, "(//a[@href='/ipad/'])[1]")
+navigatinBarIphone = (By.XPATH, "(//a[@href='/iphone/'])[1]")
+navigatinBarWatch = (By.XPATH, "(//a[@href='/watch/'])[1]")
+navigatinBarAirPods = (By.XPATH, "(//a[@href='/airpods/'])[1]")
+navigatinBarTVHome = (By.XPATH, "(//a[@href='/tv-home/'])[1]")
+navigatinBarOnlyOnApple = (By.XPATH, "(//a[@href='/services/'])[1]")
+navigatinBarWhereToBuy = (By.XPATH, "(//a[@href='/buy/'])[1]")
+
+navigatinBarBag = (By.XPATH, "(//a[@class='ac-gn-link ac-gn-link-bag'])[2]")
+navigatinBarHidnBag = (By.CLASS_NAME, "ac-gn-bagview-nav-link ac-gn-bagview-nav-link-bag")
+
+#SelectProductDetailsPage
+
+blackColor = (By.XPATH, "(//div[@class ='rc-dimension-multiple form-selector-swatch column large-6 small-6 form-selector'])[5]")
+gb64 = (By.XPATH, "(//div[@class ='rc-dimension-multiple form-selector-threeline column large-6 small-6 form-selector'])[1]")
+connectLoc = (By.XPATH, "(//div[@class ='rf-dimension-simfree rc-dimension-multiple form-selector-threeline column large-12 small-12 form-selector'])[1]")
+pressNO = (By.ID, "noTradeIn")
+oneTimePayment = (By.XPATH, "(//div[@class ='rc-dimension-selector-row form-selector-twocol-threeline form-selector'])[2]")
+addBag = (By.NAME, "add-to-cart")
+
---
+++ Pages/SelectProductDetailsPage/SelectProductDetailsPage.py
@@ -0,0 +1,42 @@
+import time
+from Common.CustomFind.FindElement import FindElement
+from LocatorsFile.Locators import *
+
+class SelectProductDetailsPage():
+    def __init__(self, driver):
+        self.driver = driver
+        self.FindElement = FindElement(self.driver)
+
+    def choose_color(self):
+        chooseColor = self.FindElement.find(*blackColor)
+        chooseColor.click()
+        time.sleep(1)
+
+    def choose_gb(self):
+        chooseGB = self.FindElement.find(*gb64)
+        chooseGB.click()
+        time.sleep(1)
+
+    def connect_on_your_own_later(self):
+        clickOnIt = self.FindElement.find(*connectLoc)
+        clickOnIt.click()
+        time.sleep(1)
+
+    def press_no(self):
+        pressNo = self.FindElement.find(*pressNO)
+        pressNo.click()
+        time.sleep(1)
+
+    def one_time_payment(self):
+        oneTimePay = self.FindElement.find(*oneTimePayment)
+        oneTimePay.click()
+
+    def scroll(self):
+        scroll = self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight, 500)")
+        return scroll
+
+    def add_to_bag(self):
+        time.sleep(2)
+        addToBag = self.FindElement.find(*addBag)
+        addToBag.click()
+
