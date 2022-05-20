5f141726576f8a7a992fc64b4d24405c5b7ec12d
Garik-Avetisyan <88md707@gmail.com>
2022-05-20 14:17:45 -0400

    git action from pycharm

--- Common/GeneralSetUp/SetUpFile.py
+++ Common/GeneralSetUp/SetUpFile.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+# Class for driver setup
 class Set_Up_Class():
     def generalSetUp(self):
         self.driver = webdriver.Chrome("..\\Common\\Drivers\\chromedriver.exe")
